6e3faf2c60b5424c9f78c06d416dd88d7b74804c
sunil8390 <sunilpawar8390@gmail.com>
2022-03-07 17:17:10 +0530

    merge two or more json record im map

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import  React,{useState} from 'react';
 import './BookList.css' ;
 import { Button, Form} from 'react-bootstrap';
 import Popup from 'reactjs-popup';
@@ -6,63 +6,148 @@ import 'reactjs-popup/dist/index.css';
 
 
 
+
+
  const BookList = (props)=> {
 
-    const  getBook =props.bookData;
+    const  getBook = props.bookData;
     const  getCategory = props.categoriesData;
     const  getPublisher = props.publisherData;
 
-    const editBookDetails = ()=>{
-       console.log(getBook);
+    const [bookName, setBookName]= useState('');
+   
+    const bookChangeEvent = (event) => {
+     
+        setBookName( event.target.value);
+        event.preventDefault();
+        console.log(bookName);
     }
-    
-    const deleteBookDetails = ()=>{
-        alert(`this is delete book alert`);
+
+   
+    const [list, updateList] = useState(getBook);
+
+    const handleRemove = (BookId) => {
+     const newList  = list.filter((bk) => bk.BookId !== BookId);
+      updateList(newList);
+      console.log(list);
+      
     }
+   
 
 
-    // console.log(props.bookData)
-    // console.log(props.categoriesData)
-    // console.log(props.publisherData)
 
-    const displayBookData=getBook.map(
-        (bk)=>{
-            return(
-                <tbody>
-                 <tr>                  
-                    <td>{bk.BookId }</td>
-                    <td>{bk.BookName }</td>
-                    <td>{bk.CategoriesId }</td>
-                    <td>{bk.PublisherId }</td>
-                    <td>{bk.Quantity }</td> 
-                    <td>
-                        <Popup  trigger={<Button > Edit</Button>} position="right center" >
+
+    //BookDetails 
+
+    // const displayBookData=getBook.map(
+    //     (bk)=>{ 
+    //         return(  
+             
+              
+    //             <tbody>
+    //              <tr key={bk.BookId} >  
+               
+    //                 <td>{bk.BookId }</td>
+    //                 <td>{bk.BookName }</td>
+    //                 <td>{bk.CategoriesId }</td>
+    //                 <td>{bk.PublisherId }</td>
+    //                 <td>{bk.Quantity }</td> 
+    //                 <td>
+    //                     <Popup  trigger={<Button> Edit</Button>}  position="right center" >
+    //                         <div >
+    //                         <Form >
+    //                             <Form.Group className="mb-3" controlId="formBasicEmail">
+    //                                 <Form.Label>BookID</Form.Label>
+    //                                 <Form.Control value={bk.BookId}  disabled="disabled" type="text" />
+                                    
+    //                             </Form.Group>
+
+    //                             <Form.Group className="mb-3" controlId="formBasicPassword">
+    //                                 <Form.Label>BookName</Form.Label>
+    //                                 <Form.Control value={bk.BookName} onChange={bookChangeEvent} type="text"  />
+                                   
+    //                             </Form.Group>
+
+    //                             <Form.Group className="mb-3" controlId="formBasicPassword">
+    //                                 <Form.Label>CategoriesId</Form.Label>
+    //                                 <Form.Control value={bk.CategoriesId} type="text"  />
+    //                             </Form.Group>
+
+    //                             <Form.Group className="mb-3" controlId="formBasicPassword">
+    //                                 <Form.Label>PublisherId</Form.Label>
+    //                                 <Form.Control  value={bk.PublisherId} type="text"  />
+    //                             </Form.Group>
+
+    //                             <Form.Group className="mb-3" controlId="formBasicPassword">
+    //                                 <Form.Label>Quantity</Form.Label>
+    //                                 <Form.Control value={bk.Quantity} type="text" />
+    //                             </Form.Group>
+                                
+    //                             <Button variant="primary" type="submit">
+    //                                 Update
+    //                             </Button>
+    //                         </Form>
+
+    //                         </div>
+    //                     </Popup>
+                        
+    //                  </td>
+    //                 <td><Popup  trigger={<Button > Delete</Button>} position="right center" >
+    //                         <div >
+    //                         <h4>{ "are you sure you want to delete book: " + " " + bk.BookName }</h4>
+    //                             <Button onClick={() => handleRemove(bk.BookId)}>Comfirm</Button>                               
+    //                         </div>
+    //                     </Popup></td>  
+                    
+    //              </tr>
+    //              </tbody>
+                           
+    //         )
+    //     }
+    // )
+
+    //nested book show
+
+    const rows = (
+        getBook
+          .map(x => Object.assign(x, getCategory.find(y => y.CategoriesID === x.CategoriesId)))
+          .map(x => Object.assign(x, getPublisher.find(z => z.PublisherId === x.PublisherId)))
+          .map(info => (
+              <tr key={info.BookId}>
+                <td>{info.BookId}</td>
+                <td>{info.BookName}</td>
+                <td>{info.CategoriesName}</td>
+                <td>{info.PublisherName}</td>
+                <td>{info.Quantity}</td>
+                <td>
+                        <Popup  trigger={<Button> Edit</Button>}  position="right center" >
                             <div >
                             <Form >
                                 <Form.Group className="mb-3" controlId="formBasicEmail">
                                     <Form.Label>BookID</Form.Label>
-                                    <Form.Control type="text" />
+                                    <Form.Control value={info.BookId}  disabled="disabled" type="text" />
                                     
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>BookName</Form.Label>
-                                    <Form.Control type="text"  />
+                                    <Form.Control value={info.BookName} onChange={bookChangeEvent} type="text"  />
+                                   
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>CategoriesId</Form.Label>
-                                    <Form.Control type="text"  />
+                                    <Form.Label>CategoriesName</Form.Label>
+                                    <Form.Control value={info.CategoriesName} type="text"  />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                                    <Form.Label>PublisherId</Form.Label>
-                                    <Form.Control type="text"  />
+                                    <Form.Label>PublisherName</Form.Label>
+                                    <Form.Control  value={info.PublisherName} type="text"  />
                                 </Form.Group>
 
                                 <Form.Group className="mb-3" controlId="formBasicPassword">
                                     <Form.Label>Quantity</Form.Label>
-                                    <Form.Control type="text" />
+                                    <Form.Control value={info.Quantity} type="text" />
                                 </Form.Group>
                                 
                                 <Button variant="primary" type="submit">
@@ -71,16 +156,20 @@ import 'reactjs-popup/dist/index.css';
                             </Form>
 
                             </div>
-                        </Popup> 
+                        </Popup>
+                        
                      </td>
-                    <td><Button onClick={deleteBookDetails}>Delete</Button></td>  
-                    
-                 </tr>
-                 </tbody>
-                           
+                    <td><Popup  trigger={<Button > Delete</Button>} position="right center" >
+                            <div >
+                            <h4>{ "are you sure you want to delete book: " + " " + info.BookName }</h4>
+                                <Button onClick={() => handleRemove(info.BookId)}>Comfirm</Button>                               
+                            </div>
+                        </Popup></td>
+               
+              </tr>
             )
-        }
-    )
+          )
+      )
 
 
     //categoriesData
@@ -89,25 +178,27 @@ import 'reactjs-popup/dist/index.css';
         (ct)=>{
             return(
                 <tbody>
-                    <tr>
+                    <tr key={ct.CategoriesID}>
                     <td>{ct.CategoriesID }</td>
                     <td>{ct.CategoriesName }</td> 
+                    
+                                        
                     <td><Button>Edit</Button></td>
-                    <td><Button>Delete</Button></td>  
-
-                    </tr>                   
+                    <td><Button>Delete</Button></td>   
+                </tr>              
                 </tbody>
             )
         }
     )
 
-// publisherDeatils
+    // publisherDeatils
 
-const displayPublisherData=getPublisher.map(  
+    const displayPublisherData=getPublisher.map(  
     (pub)=>{
         return(
             <tbody>
-                <tr>
+                <tr key={pub.PublisherId}>
+                
                 <td>{pub.PublisherId }</td>
                 <td>{pub.PublisherName }</td>
                 <td><Button>Edit</Button></td>
@@ -122,49 +213,48 @@ const displayPublisherData=getPublisher.map(
 
 
 
+
+
   return (
-    <div className="App">
+   <div className="App">
         
-     <h1 className="headName">Book List</h1>
+        <h1 className="headName">Book List</h1>
 
-     <h3>BookDetails</h3>
+            <h3 >BookDetails</h3>
      
-   <table class="flat-table flat-table-1">
-            <thead>
-                <tr>
-                    <th>BookId</th>
-                    <th>BookName</th>
-                    <th>CategoriesId</th>
-                    <th>PublisherId</th>
-                    <th>Quantity</th>
-                </tr>
-             </thead>
-
-
-                
-                    {displayBookData}   
-                
-
-
+            <table className="flat-table flat-table-1">
+       
+                <thead>
+                    <tr>
+                        <th>BookId</th>
+                        <th>BookName</th>
+                        <th>CategoriesName</th>
+                        <th>PublisherName</th>
+                        <th>Quantity</th>
+                    </tr>
+                </thead>            
+                        {/* {displayBookData}  */}
+                        {rows}
+                       
+                          
+        
             </table>
+
+            
            
             <h3>CategoriesDetails</h3>
-            <table class="flat-table flat-table-2">
+            <table className="flat-table flat-table-2">
 
                 <thead>
                    <th>CategoriesID</th>
                     <th>CategoriesName</th>  
-                </thead>
-
-              
-                     {displayCategoriesData}
-                
-                       
-                
+                </thead>      
+                       {displayCategoriesData}
+                          
             </table>
 
             <h3>PublisherDeatils</h3>
-            <table class="flat-table flat-table-3">
+            <table className="flat-table flat-table-3">
 
                 <thead>
                     <th>PublisherId</th>
@@ -174,13 +264,15 @@ const displayPublisherData=getPublisher.map(
                
                     {displayPublisherData}
                 
-          </table>
+            </table>
 
           
       
-          </div>
+    </div>
    
   );
 }
+
+
 
 export default BookList;
